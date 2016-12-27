e176dd0b1bd7e48d99f2b66f06252167da9d6691
zhangxuchao <2212362272@qq.com>
2016-12-27 16:52:00 +0800

    Create chart.ts

---
+++ chart.ts
@@ -0,0 +1,14 @@
+1. 安装 ng2-charts
+npm install ng2-charts --save
+
+2. 还必须引入 chart.js
+npm install chart.js --save
+
+3. 主模块中引入
+import { ChartsModule } from 'ng2-charts/ng2-charts';
+imports: [
+   ChartsModule
+]
+
+4. 然后就可以使用了参考api文档
+http://valor-software.com/ng2-charts/
